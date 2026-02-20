64adab17333c6984cba02ca014e40d86404d8978
Nikita Volkov <volchonik634@gmail.com>
2026-02-20 10:33:59 +0200

    Set up Worker

--- PandemicShield.Worker/Program.cs
+++ PandemicShield.Worker/Program.cs
@@ -1,10 +1,44 @@
-﻿namespace PandemicShield.Worker
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace PandemicShield.Worker
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            var factory = new ConnectionFactory() { HostName = "localhost"};
+            using var connection = await factory.CreateConnectionAsync();
+            using var channel = await connection.CreateChannelAsync();
+            {
+                await channel.QueueDeclareAsync(
+                        queue: "dna_chunks",
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false
+            );
+
+                var consumer = new AsyncEventingBasicConsumer(channel);
+
+                consumer.ReceivedAsync += async (model, ea) =>
+                {
+                    byte[] body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine($" [x] Отримано кількість: {message.Length}");
+
+                    await channel.BasicAckAsync(deliveryTag: ea.DeliveryTag, multiple: false);
+
+                };
+
+                await channel.BasicConsumeAsync(queue: "dna_chunks", autoAck: false, consumer: consumer);
+
+                Console.ReadLine();
+            }
+
+
         }
     }
 }
